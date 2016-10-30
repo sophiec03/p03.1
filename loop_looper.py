ecd6af978cb9fb809ec7d70a99d048b34ffddba2
sophiec03 <sophie.sc2@gmail.com>
2016-10-30 21:36:22 +0000

    loop looper

--- loop_looper.py
+++ loop_looper.py
@@ -26,3 +26,11 @@ def run_tests():
 
 # Edit this code
 def looper(n):
+    
+    loop = 0
+    for i in range(1, n+1):
+        for j in range(1, n+1:
+            
+            loop = loop + (i * j)
+                       
+    print(loop)
